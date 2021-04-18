c35cdab133177ab0d4053fc902c238b219e70860
Benyamin <beneslami72@gmail.com>
2021-04-19 00:29:02 +0200

    spatial/temporal distribution

--- spatialdist.py
+++ spatialdist.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 chiplet = []
 chiplet_0 = dict(
     SM_ID=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
@@ -13,8 +14,8 @@ chiplet_1 = dict(
 link_12 = 0
 chiplet_2 = dict(
     SM_ID=[64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-           91, 92, 93, 94],
-    LLC_ID=[160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174], port=194)
+           91, 92, 93, 94, 95],
+    LLC_ID=[160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175], port=194)
 link_23 = 0
 chiplet_3 = dict(
     SM_ID=[96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117,
@@ -28,9 +29,18 @@ chiplet.append(chiplet_3)
 
 
 def chip_select(num):
+    out = -1
     for i in range(len(chiplet)):
-        if num in chiplet[i]["SM_ID"] or num in chiplet[i]["LLC_ID"] or num == chiplet[i]["port"]:
-            return i
+        if num in chiplet[i]["SM_ID"]:
+            out = i
+            break
+        if num in chiplet[i]["LLC_ID"]:
+            out = i
+            break
+        if num == chiplet[i]["port"]:
+            out = i
+            break
+    return out
 
 
 def check_local_or_remote(x):  # 0 for local, 1 for remote
@@ -41,8 +51,47 @@ def check_local_or_remote(x):  # 0 for local, 1 for remote
         return 1
 
 
+def injeciton_flow(packet):
+    arr = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    for i in packet.keys():
+        for j in range(len(packet[i])):
+            if packet[i][j][0] == "injection buffer" and int(packet[i][j][4].split(": ")[1]) == 0:
+                src = chip_select(int(packet[i][j][1].split(": ")[1]))
+                dst = chip_select(int(packet[i][j][2].split(": ")[1]))
+                if src == -1 or dst == -1:
+                    print(int(packet[i][j][1].split(": ")[1]))
+                    print(int(packet[i][j][2].split(": ")[1]))
+                arr[3 - src][dst] += 1
+                continue
+            elif packet[i][j][0] == "L2_icnt_pop" and int(packet[i][j][4].split(": ")[1]) == 2:
+                src = chip_select(int(packet[i][j][1].split(": ")[1]))
+                dst = chip_select(int(packet[i][j][2].split(": ")[1]))
+                if src == -1 or dst == -1:
+                    print(int(packet[i][j][1].split(": ")[1]))
+                    print(int(packet[i][j][2].split(": ")[1]))
+                arr[3 - src][dst] += 1
+                break
+
+    Index = [0, 1, 2, 3]
+    Cols = [3, 2, 1, 0]
+    plt.xticks(ticks=np.arange(len(Index)), labels=Index)
+    plt.yticks(ticks=np.arange(len(Cols)), labels=Cols)
+    hm = plt.imshow(arr, cmap='gray_r', interpolation="nearest")
+    plt.colorbar(hm)
+    plt.xlabel("Module")
+    plt.ylabel("Module")
+    plt.title("spatial Distribution of Injection Flows")
+    plt.show()
+
+
+def injection_rate(packet):
+ # TODO: injection interarrival time
+
+def hop_latency_dist(packet):
+    # todo: temporal distribution of injection between each neighbors
+
 if __name__ == "__main__":
-    file = open("icnt.txt", "r")
+    file = open("report.txt", "r")
     raw_content = ""
     if file.mode == "r":
         raw_content = file.readlines()
@@ -55,20 +104,13 @@ if __name__ == "__main__":
 
     for i in range(len(lined_list)):
         if int(lined_list[i][3].split(": ")[1]) in packet.keys():
-            packet.setdefault(int(lined_list[i][3].split(": ")[1]), []).append(lined_list[i])
+            if lined_list[i] not in packet[int(lined_list[i][3].split(": ")[1])]:
+                packet.setdefault(int(lined_list[i][3].split(": ")[1]), []).append(lined_list[i])
         else:
             packet.setdefault(int(lined_list[i][3].split(": ")[1]), []).append(lined_list[i])
+    """
+    for i in range(len(packet[15803589])):
+        print(packet[15803589][i])
+    """
+    injeciton_flow(packet)
 
-    arr = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    for i in packet.keys():
-        src = int(packet[i][0][1].split(": ")[1])
-        dst = int(packet[i][0][2].split(": ")[1])
-        arr[3 - chip_select(src)][chip_select(dst)] += 1
-
-    Index = [0, 1, 2, 3]
-    Cols = [3, 2, 1, 0]
-    plt.xticks(ticks=np.arange(len(Index)), labels=Index)
-    plt.yticks(ticks=np.arange(len(Cols)), labels=Cols)
-    hm = plt.imshow(arr, cmap='gray_r', interpolation="nearest")
-    plt.colorbar(hm)
-    plt.show()
