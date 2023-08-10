d40d7c18f1e18a31cff64a771438616a3f6eabd7
ben <beneslami72@gmail.com>
2023-08-10 11:34:46 +0200

    improvement

--- characterizing_benchmarks/main.py
+++ characterizing_benchmarks/main.py
@@ -1,5 +1,6 @@
 import gc
 import os
+import benchlist
 from pathlib import Path
 from check_benchmark_traces import *
 from re_arrange_benchmark import *
@@ -8,22 +9,11 @@ from trace_generator import *
 
 
 if __name__ == "__main__":
-    suits = ['ispass-2009', 'pannotia', 'parboil', 'polybench', 'rodinia', 'shoc', 'tango']
-    benchmarks = {
-        'ispass-2009': ['bfs', 'NQU', 'STO'],
-        'pannotia': ['color-max', 'fw'],
-        'parboil': ['bfs', 'cutcp', 'histo', 'mri-gridding', 'sgemm', 'spmv', 'stencil'],
-        'polybench': ['2DConvolution', '2mm', '3mm', 'atax', 'bicg', 'correlation', 'covariance', 'fdtd2d', 'gemm',
-                      'gesummv', 'mvt', 'syr2k', 'syrk'],
-        'rodinia': ['backprop', 'bfs', 'b+tree', 'cfd', 'gaussian', 'heartwall', 'hotspot', 'hotspot3D', 'huffman',
-                     'hybridsort', 'kmeans', 'lavaMD', 'lud', 'nn', 'particlefilter-float', 'pathfinder', 'srad',
-                    'streamcluster'],
-        'shoc': ['fft', 'gemm', 'scan', 'Spmv'],
-        'tango': ['AlexNet', 'ResNet', 'SqueezeNet']
-    }
-    topology = ['torus', 'ring', 'mesh', 'fly']
-    NVLink = ['NVLink4', 'NVLink3', 'NVLink2', 'NVLink1']
-    chiplet_num = ['4chiplet', '8chiplet', '16chiplet']
+    suits = benchlist.suits
+    benchmarks = benchlist.benchmarks
+    topology = benchlist.topology
+    NVLink = benchlist.NVLink
+    chiplet_num = benchlist.chiplet_num
 
     path = "/home/ben/Desktop/benchmarks/"
     for suite in suits:
